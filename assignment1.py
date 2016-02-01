0bdf41e08dc4775cda30170e665498928a078284
cmis <cmis@cmis-hs13.(none)>
2016-02-01 13:34:22 +0700

    edit

--- assignment1.py
+++ assignment1.py
@@ -1,9 +1,13 @@
 Name = "Julia"
-Age = 16
-Height = 167
-LS = 5
+Age = 16.25
+Height = 1.67
+SquareLength = 5
 LR = 9
 HR = 4
-
-leftYears = 23 
+MonthAge = Age + 0.83 
+LefttoLive = 73.2
+HeightFeet = Height * 3.28
+HeighDifference = Height - 160.1
+SquareArea = SquareLength * 4
+HalfVolume = SquareLength **3/2
 
