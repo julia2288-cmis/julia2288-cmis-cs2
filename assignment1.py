b4c8f57740523fa2a17b87e40c3a7b91f3a8d4c4
cmis <cmis@cmis-hs13.(none)>
2016-02-03 08:43:49 +0700

    edit

--- assignment1.py
+++ assignment1.py
@@ -1,13 +1,17 @@
-Name = "Julia"
-Age = 16.25
-Height = 1.67
-SquareLength = 5
-LR = 9
-HR = 4
-MonthAge = Age + 0.83 
-LefttoLive = 73.2
-HeightFeet = Height * 3.28
-HeighDifference = Height - 160.1
-SquareArea = SquareLength * 4
-HalfVolume = SquareLength **3/2
-
+Name = "Julia" # variable for my name.
+Age = 16.25 # variable for my age.
+Height = 1.67 # variable for my height.
+SquareLength = 5 # variable for length of a square.
+RectangleLength = 9 # variable for length of a rectangle.
+RectangleHeight= 4 # variable for height of a rectangle.
+MonthAge = Age + 0.83 # variable for my age in month.
+LefttoLive = 73.2 # variable for my years left to live.
+HeightinFeet = Height * 3.28 # variable for my height in feet.
+HeighDifference = Height - 160.1 # variable for my height subtracted by average height.
+SquareArea = SquareLength * 4 # variable for area of a square.
+HalfVolume = SquareLength **3/2 # variable for half of volume of a square.
+RectangleArea = RectangleLength * RectangleHeight / 1/9 # 1/9 of the area of a rectangle.
+print "Hello. My name is " + str(Name) + ". My age is " + str(Age) + ", which is about " + str(MonthAge) + " in month. I can live about " + str(LefttoLive) + " more years. My height is about " + str(Height) + " meters."
+# print out sentence by using 5 variables
+print "Hello. My name is", Name,". My age is", Age, "which is about" MonthAge, ". I can live about", LefttoLive, "more years. My height is about", Height, "meters." # print out sentence by using 5 variables.
+print ";)"*10000 # smile ;)
