069ddab44c8b09b9c603af82dea5d1c3f71d0d66
cmis <cmis@cmis-hs13.(none)>
2016-02-10 10:55:39 +0700

    edit

--- assignment1.py
+++ assignment1.py
@@ -1,17 +1,31 @@
-Name = "Julia" # variable for my name.
-Age = 16.25 # variable for my age.
-Height = 1.67 # variable for my height.
-SquareLength = 5 # variable for length of a square.
-RectangleLength = 9 # variable for length of a rectangle.
-RectangleHeight= 4 # variable for height of a rectangle.
-MonthAge = Age + 0.83 # variable for my age in month.
-LefttoLive = 73.2 # variable for my years left to live.
-HeightinFeet = Height * 3.28 # variable for my height in feet.
-HeighDifference = Height - 160.1 # variable for my height subtracted by average height.
-SquareArea = SquareLength * 4 # variable for area of a square.
-HalfVolume = SquareLength **3/2 # variable for half of volume of a square.
+Name = "Julia" 
+# variable for my name.
+Age = 16.25 
+# variable for my age.
+Height = 1.67 
+# variable for my height.
+SquareLength = 5 
+# variable for length of a square.
+RectangleLength = 9 
+# variable for length of a rectangle.
+RectangleHeight= 4 
+# variable for height of a rectangle.
+MonthAge = Age + 0.83 
+# variable for my age in month.
+LefttoLive = 73.2 
+# variable for my years left to live.
+HeightinFeet = Height * 3.28 
+# variable for my height in feet.
+HeighDifference = Height - 160.1 
+# variable for my height subtracted by average height.
+SquareArea = SquareLength * 4 
+# variable for area of a square.
+HalfVolume = SquareLength **3/2 
+# variable for half of volume of a square.
 RectangleArea = RectangleLength * RectangleHeight / 1/9 # 1/9 of the area of a rectangle.
 print "Hello. My name is " + str(Name) + ". My age is " + str(Age) + ", which is about " + str(MonthAge) + " in month. I can live about " + str(LefttoLive) + " more years. My height is about " + str(Height) + " meters."
 # print out sentence by using 5 variables
-print "Hello. My name is", Name,". My age is", Age, "which is about" MonthAge, ". I can live about", LefttoLive, "more years. My height is about", Height, "meters." # print out sentence by using 5 variables.
-print ";)"*10000 # smile ;)
+print "Hello. My name is", Name,". My age is", Age, "which is about", MonthAge, ". I still can live ", LefttoLive, "more years. My height is about", Height, "meters." 
+# print out sentence by using 5 variables.
+print ";)"*10000 
+# smile ;)
--- functions.py
+++ functions.py
@@ -1,2 +1,22 @@
 def add(a,b):
-
+	return a + b	
+	add(3,4)
+#addition
+def sub(a,b):
+	return a - b
+	sub(5,3)
+#subtaction
+def mul(a,b):
+	return a * b
+	mul(4,4)
+#multiplication
+def div(a,b):
+	return a / b
+	div(2,3)
+#division
+def hours_from_seconds(a,b,c):
+	return a / b / c
+	hours_from_seconds(86400,60,60)
+#number of seconds and returns the number of hours.
+def circle_area
+1.59154943092
