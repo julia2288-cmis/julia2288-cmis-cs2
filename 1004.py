f5ed7871a6a77c2ff6cd0d793418f76abf8bda16
cmis <cmis@cmis-hs13.(none)>
2016-03-14 10:55:37 +0700

    added

---
+++ 1004.py
@@ -0,0 +1,9 @@
+def output():	
+	The target was 
+	
+	
+def main(number):
+	raw_input("What is the minimum number?: ")
+	raw_input("What is the maximum number?: ")
+	raw_input("I'm thinking of a number from " "to ")
+	raw_input("What do you think it is?: ")
