a7ab17abb576a554dc707a93382fcad16bbf8650
cmis <cmis@cmis-hs13.(none)>
2016-03-15 13:36:36 +0700

    edit

--- 1004.py
+++ 1004.py
@@ -1,9 +1,26 @@
-def output():	
-	The target was 
-	
+def float():
+	if c >= a
+	if b >= c
+	return True 
+ else:
+        strength = random.randint(0,10)
+    return strength * random.random()
+
+
+def output(a, b, c):	
+	return """
+The target was {}.
+Your guess was {}.
+That's under by  {}.
+""".format
 	
 def main(number):
 	raw_input("What is the minimum number?: ")
 	raw_input("What is the maximum number?: ")
 	raw_input("I'm thinking of a number from " "to ")
 	raw_input("What do you think it is?: ")
+
+out = output(a, b, c)
+print out
+
+main(number):
