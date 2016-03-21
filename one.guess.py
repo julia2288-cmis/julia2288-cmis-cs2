183058c2936dfa4166f47e266a008e386aa17d5e
cmis <cmis@cmis-hs20.(none)>
2016-03-21 10:34:25 +0700

    edit

---
+++ one.guess.py
@@ -0,0 +1,29 @@
+import random
+
+def float(minimum,maximum):
+	return strength * random.random(minimum,maximum)
+
+def PlayerResult(result, guess):
+    if result > guess:
+        sub = abs(guess - result)
+        return "That is under by {}".format(sub)
+    else:
+        add = abs(guess - result)
+        return "That is over by {}".format(add)
+
+def PlayerResult(result, PlayersGuess):
+    if result > PlayersGuess:
+        sub = abs(PlayersGuess - result)
+        return "That is under by {}".format(sub)
+    else:
+        add = abs(PlayersGuess - result)
+        return "That is over by {}".format(add)
+
+def main(number):
+	raw_input("What is the minimum number?: ")
+	raw_input("What is the maximum number?: ")
+	raw_input("I'm thinking of a number from " minimum "to " maximum)
+	raw_input("What do you think it is?: ")
+
+main()
+
