52150d20b66ffadb9fa4132fb95217d1c73d2aaf
cmis <cmis@cmis-hs20.(none)>
2016-03-29 13:17:15 +0700

    edit

--- one.guess.py
+++ one.guess.py
@@ -22,7 +22,7 @@ def PlayerResult(result, PlayersGuess):
 def main(number):
 	raw_input("What is the minimum number?: ")
 	raw_input("What is the maximum number?: ")
-	raw_input("I'm thinking of a number from " minimum "to " maximum)
+	raw_input("I'm thinking of a number from " "to " )
 	raw_input("What do you think it is?: ")
 
 main()
