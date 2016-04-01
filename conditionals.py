402868a899a1406af9cff37b8f84ac2ddb56c8c9
cmis <cmis@cmis-hs08.(none)>
2016-04-01 10:55:21 +0700

    add

--- conditionals.py
+++ conditionals.py
@@ -1,2 +1,30 @@
-def numbers():
+def numbers(playernumber,computernumber):
+	if playernumber == float(31):
+		print "You lost!"
+	elif computernumber == float(31):
+		print "You win!"
+
+def numbers(A, B, C):
+	A == float(A)
+	B == float(A+1)
+	C == float(B+1)	
+
+def numbers(playernumber,computernumber):	
+	playernumber ==  float(A, B, C)
+	if playnumber == float
+	 
 	
+
+def main(numbers):
+	raw_input("How many numbers you want to type?")
+	raw_input
+	raw_input
+	
+	output = """
+You typed {},{},{}.
+My number is {},{},{}.
+""".format(A, B, C)
+
+
+
+
