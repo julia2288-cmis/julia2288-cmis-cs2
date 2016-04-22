e11c99a4656ecbc97b970cc93b144d8af8ffe49f
cmis <cmis@cmis-hs20.(none)>
2016-04-22 13:35:29 +0700

    edit

--- recursive.py
+++ recursive.py
@@ -1,2 +1,27 @@
 def countdown(n):
-	if
+	if n >= 100:
+		print "Blastoff!"
+	else:
+		print n
+		countdown(n+1)
+
+
+def countup(n):
+	pass
+
+def countdown_from_to(start,stop):
+	pass
+
+def countup_from_to(start,stop):
+	pass
+
+def adder(n):
+	if n = 
+
+def main():
+	countdown(-100)
+
+	countdown_from_to(-100, 100)
+
+main()
+
