caa39e662b722b2dd925052571392fbafca9cc2a
cmis <cmis@cmis-hs20.(none)>
2016-04-28 13:35:28 +0700

    edit

--- recursive.py
+++ recursive.py
@@ -1,27 +1,41 @@
-def countdown(n):
-	if n >= 100:
+#def countdown(n):
+#	if n >= 100:
+#		print "Blastoff!"
+#	else:
+#		print n
+#		countdown(n-1)
+
+
+#def countup(n):
+#	if n >= 100:
+#		print "Blastoff!"
+#	else:
+#		print n
+#		countup(n+1)
+	
+def adder(running_total = 0):
+	print "The running total is {}".format(running_total)
+	n0 = raw_input("next number: ")
+	if n0 == "":
+		print "The sum is {}".format(running_total)
+	else:
+		running_total += float(n0)
+
+		adder(running_total)
+
+
+def countup_from_to(start, stop):
+	if start >= stop:
 		print "Blastoff!"
 	else:
-		print n
-		countdown(n+1)
+		print start
+		countup(start+1)
 
-
-def countup(n):
-	pass
-
-def countdown_from_to(start,stop):
-	pass
-
-def countup_from_to(start,stop):
-	pass
-
-def adder(n):
-	if n = 
+def countdown_from_to(start, stop):
+	if start <= stop:
+		print "Blastoff!"
+	else:
+		print start
+		countdown(start-1)
 
 def main():
-	countdown(-100)
-
-	countdown_from_to(-100, 100)
-
-main()
-
