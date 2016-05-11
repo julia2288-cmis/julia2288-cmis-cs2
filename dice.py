05eba406673cf8599e3aa624d7438d4b344b3494
cmis <cmis@cmis-hs20.(none)>
2016-05-11 13:35:41 +0700

    edit

--- dice.py
+++ dice.py
@@ -1,7 +1,13 @@
-def rules():
-	print "This is just like snakes and ladders. You can chose how many steps you will go forward. There will be some traps in few numbers.Good luck!"
+import random
 
-def steps():
-	print "You are in number {}".format(total)	
-	n = raw_input("How many steps you want to go?")
-	if n = 
+def rules():
+	print "This is just like snakes and ladders. If you press enter, computer will roll the dice. You have to go forward with the given number. There will be some traps in few steps.Good luck!"
+
+def steps():	
+	n = raw_input("Click enter to roll the dice.")	
+	float(dice) = 	random.randint(1,6) 
+	if n == "":
+		return float(dice)
+	print "You are in number {}.".format(float(dice))
+	
+
