c6aed26fb7052ae26b67b453e5360b410e4dab89
julia2288-cmis <julia2288@cmis.ac.th>
2016-05-15 20:37:52 +0700

    Update dice.py

--- dice.py
+++ dice.py
@@ -24,12 +24,11 @@ def traps2():
 	n = steps()
 	if n == float(9,26,73,81)
 		print "You stepped on a banana peel. You will slip 3 steps back." 
-		return sub(n-3)
+		return abs(float(sub(n-3)))
 
 
 def traps3():
 	n = steps()
-	if n == float(6,13,39,69)
-		print "You stepped on a snake and snake bite you. You will start from the beginning again."
-		return steps()
-
+	if n == float(28,43,96)
+		print "You found an escalator infront of you. You will skip 4 steps to the front."
+		return abs(float(add(n+4)))
