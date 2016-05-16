e10d42f781ae17eec007eca869dfbd29adf08201
cmis <cmis@cmis-hs20.(none)>
2016-05-16 10:57:23 +0700

    edit

--- dice.py
+++ dice.py
@@ -3,38 +3,41 @@ import random
 def rules():
 	print "This is just like snakes and ladders. If you press enter, computer will roll the dice. You have to go forward with the given number. There will be some traps in few steps.Good luck!"
 
-def steps():	
-	n = raw_input("Click enter to roll the dice.")	
-	float(dice) = random.randint(1,6) 
-	if n == "":
-		return float(dice)
-	print "You are in number {}.".format(float(dice))
-	c = raw_input("Type enter to continue: ")
-	elif c == "":
-		return steps() 
+def steps(dice):	
+	dice = raw_input("Click enter to roll the dice.")	
+	if dice == "":
+		return random.randint(1,6) 
+	print "You are in number {}.".format(dice)
+	
+def next(c):
+	c = raw_input("Type enter to continue: ")	
+	if c == "":
+		return steps(dice) 
 
 def traps1():
-	n = steps()
-	if n == float(6,13,39,69)
+	n = steps(dice)
+	if n == float(6,13,39,69,70,24,36,99,4,14):
 		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
-		return steps()
+		return steps(dice)
 
 
 def traps2():
-	n = steps()
-	if n == float(9,26,73,81)
+	n = steps(dice)
+	if n == float(9,26,33,35,21,2,73,81,65):
 		print "You stepped on a banana peel. You will slip 3 steps back." 
 		return abs(float(sub(n-3)))
 
 
 def traps3():
-	n = steps()
-	if n == float(28,43,96)
+	n = steps(dice)
+	if n == float(28,43,58,62,10,96,7,34,23,78):
 		print "You found an escalator infront of you. You will skip 4 steps to the front."
 		return abs(float(add(n+4)))
 
 def main():
-	n = steps()
-	if n == float(100)
+	steps(dice)
+	n = steps(dice)
+	if n == float(100):
 		print "You won!"
+	
 main()
