f459ba2b9dec74316eb0b7ed1ac4bde37ccaa2a8
cmis <cmis@cmis-hs00.(none)>
2016-05-16 15:17:57 +0700

    edit

--- conditionals.py
+++ conditionals.py
@@ -1,28 +1,42 @@
+import random
+import math
+
 def rules():
-	print "You can call 1~3 numbers at your turn, and so does the computer. Whoever calls the number 31 lost."
+	print "This is just like snakes and ladders. If you press enter, computer will roll the dice. You have to go forward with the given number. There will be some traps in few steps.Good luck!"
 
-def numbers(playernumber,computernumber):
-	if playernumber == float(31):
-		print "You lost!"
-	elif computernumber == float(31):
-		print "You win!"
+def steps():	
+	dice = raw_input("Click enter to roll the dice.")	
+	if dice == "":
+		print random.randint(1,6) 
+		n = dice +
+	print "You are in number {}.".format(n)
+	
 
-def starts(turn):
-	starts(turn) = raw_input("Who starts first? player/computer")
-	if starts(turn) = "player":
-		print raw_input("Type numbers")
-	else:
-		print random.randint(1,3)
-
-def numbers():
-	numbers() = raw_input("How many numbers you want to type? (1~3)")
-	n = raw_input("What is your number?")
-	if numbers() = 1 
-		return n = numbers()
-		print random.randint(1,3)
+def next(c):
+	c = raw_input("Type enter to continue: ")	
+	if c == "":
+		return steps(dice) 
 
 
+
+def traps():
+	n = steps()
+	
+	if n == float(6,13,39,69,70,24,36,99,4,14):
+		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
+		return steps()
+
+	elif n == float(9,26,33,35,21,2,73,81,65):
+		print "You stepped on a banana peel. You will slip 3 steps back." 
+		return abs(float(sub(n-3)))
+
+
+	elif n == float(28,43,58,62,10,96,7,34,23,78):
+		print "You found an escalator in front of you. You will skip 4 steps to the front."
+		return abs(float(add(n+4)))
+
+
+ 
 def main():
-
+	steps()
 main()
-
