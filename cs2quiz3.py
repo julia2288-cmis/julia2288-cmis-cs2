4bd89a8a70037e711c00bbd7915a3f6754906e03
cmis <cmis@cmis-hs20.(none)>
2016-05-17 13:12:57 +0700

    edit

---
+++ cs2quiz3.py
@@ -0,0 +1,63 @@
+#Section 1: Terminology
+# 1) What is a recursive function?
+# base case - doesn't do anything
+#calling it again - 
+#
+# 2) What happens if there is no base case defined in a recursive function?
+#The function won't do work at all
+#
+#
+# 3) What is the first thing to consider when designing a recursive function?
+#To define a function
+#
+#
+# 4) How do we put data into a function call?
+#by input
+# 
+# 5) How do we get data out of a function call?
+#by return
+#
+#
+
+#Section 2: Reading
+# Read the following function definitions and function calls.
+# Then determine the values of the variables q1-q20.
+
+#a1 = 1 + (2,5)
+#a2 = 1 + (6,2-1) or 1 + (6,1)
+#a3 = -1
+
+#b1 = 2
+#b2 = 0
+#b3 = -2
+
+#c1 = -2
+#c2 = 4 +2 =4da,m
+#c3 =
+
+#d1 = 6
+#d2 = 1 + (6)
+#d3 = (4, 2, 1
+
+#Section 3: Programming
+#Write a script that asks the user to enter a series of numbers.
+#When the user types in nothing, it should return the average of all the odd numbers
+#that were typed in. 
+#In your code for the script, add a comment labeling the base case  on the line BEFORE the base case.
+#Also add a comment label BEFORE the recursive case.
+#It is NOT NECESSARY to print out a running total with each user input.
+
+def odd():
+	n = raw_input("Next number: ")
+	if n/2 == false 
+	else: 
+		abs(float(odd))
+
+def average():
+	n = odd()
+	if n == abs(float(odd))
+		return
+	if n == "":
+		return average
+
+
