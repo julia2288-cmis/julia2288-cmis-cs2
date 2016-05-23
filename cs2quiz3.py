fc73b9b9692abf4e9971a56b121f84658f64de4d
cmis <cmis@cmis-hs21.(none)>
2016-05-23 10:55:30 +0700

    edit

--- cs2quiz3.py
+++ cs2quiz3.py
@@ -12,7 +12,7 @@
 #
 #
 # 4) How do we put data into a function call?
-#by input
+#by 
 # 
 # 5) How do we get data out of a function call?
 #by return
@@ -23,8 +23,8 @@
 # Read the following function definitions and function calls.
 # Then determine the values of the variables q1-q20.
 
-#a1 = 1 + (2,5)
-#a2 = 1 + (6,2-1) or 1 + (6,1)
+#a1 = 1 + (2+5)
+#a2 = 1 + (6+2-1) or 1 + (6+1)
 #a3 = -1
 
 #b1 = 2
@@ -32,12 +32,12 @@
 #b3 = -2
 
 #c1 = -2
-#c2 = 4 +2 =4da,m
+#c2 = 4 +2 =4da,m wrong
 #c3 =
 
-#d1 = 6
-#d2 = 1 + (6)
-#d3 = (4, 2, 1
+#d1 = 6 wrong
+#d2 = 1 + (6) wrong
+#d3 = (4, 2, 1 wrong
 
 #Section 3: Programming
 #Write a script that asks the user to enter a series of numbers.
@@ -47,12 +47,6 @@
 #Also add a comment label BEFORE the recursive case.
 #It is NOT NECESSARY to print out a running total with each user input.
 
-def odd():
-	n = raw_input("Next number: ")
-	if n/2 == false 
-	else: 
-		abs(float(odd))
-
 def average():
 	n = odd()
 	if n == abs(float(odd))
@@ -60,4 +54,8 @@ def average():
 	if n == "":
 		return average
 
-
+def odd():
+	n = raw_input("Next number: ")
+	if n/2 == false 
+	else: 
+		abs(float(odd))
