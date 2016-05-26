4966fc69d9a3cf9a81d8a0cd24e98ac8e20513ea
cmis <cmis@cmis-hs20.(none)>
2016-05-26 08:42:53 +0700

    edit

---
+++ loops.py
@@ -0,0 +1,18 @@
+#def countFrom2(x, y):
+#	if x > y:	
+#		while x >= y:
+#			print x
+#			x -= 1
+#	elif y > x:		
+#		while x <= y:
+#			print x
+#			x += 1
+
+#countFrom2(23, 20)
+#countFrom2(2, 20)
+
+
+def addOdds():	
+	if m > 0:
+	
+	elif m < 0:
