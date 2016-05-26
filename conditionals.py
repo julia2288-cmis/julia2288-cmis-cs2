8e09d4c4b3c0ef25bebcacfb83166112b3d7cfef
cmis <cmis@cmis-hs00.(none)>
2016-05-26 15:27:17 +0700

    edit

--- conditionals.py
+++ conditionals.py
@@ -4,40 +4,31 @@ import math
 def rules():
 	print "This is just like snakes and ladders. If you press enter, computer will roll the dice. You have to go forward with the given number. There will be some traps in few steps.Good luck!"
 
-def steps():	
+def steps(total):	
 	dice = raw_input("Click enter to roll the dice.")	
+
 	if dice == "":
-		print random.randint(1,6) 
-	n = random.randint
-	n += str(dice) 	
-	return n
-	print "You are in number {}.".format(n)
-	
-
-def next(c):
-	c = raw_input("Type enter to continue: ")	
-	if c == "":
-		return steps(dice) 
-
-def traps():
-	n = steps()
-	
-	if n == float(6,13,39,69,70,24,36,99,4,14):
-		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
-		return steps()
-
-	elif n == float(9,26,33,35,21,2,73,81,65):
-		print "You stepped on a banana peel. You will slip 3 steps back." 
-		return abs(float(sub(n-3)))
-
-
-	elif n == float(28,43,58,62,10,96,7,34,23,78):
-		print "You found an escalator in front of you. You will skip 4 steps to the front."
-		return abs(float(add(n+4)))
-
-
+		print
+		n = random.randint(1,6)
+		total += n
+		print "You are in number {}.".format(total)	
+		return steps(total)
  
+
+
 def main():
-	steps()
-	n = 0
+	total = steps(0)
+	
+	if total == float(6):
+		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
+		return steps(total)
+
+	elif total == float(3):
+		print "You stepped on a banana peel. You will slip 3 steps back." 
+		return abs(float(sub(total-3)))
+
+	elif total == float(28):
+		print "You found an escalator in front of you. You will skip 4 steps to the front."
+		return abs(float(add(total+4)))
+	
 main()
