e7e5f2d4dbc3e19b9bab3398d96f6d9b21a0925c
cmis <cmis@cmis-hs02.(none)>
2016-06-02 14:25:20 +0700

    edit

--- conditionals.py
+++ conditionals.py
@@ -1,10 +1,9 @@
 import random
 import math
 
-def rules():
-	print "This is just like snakes and ladders. If you press enter, computer will roll the dice. You have to go forward with the given number. There will be some traps in few steps.Good luck!"
-
-def steps(total):	
+print "This is just like snakes and ladders. If you press enter, computer will roll the dice. You have to go forward with the given number. There will be some traps in few steps. Good luck!"
+	
+def steps(total):		
 	dice = raw_input("Click enter to roll the dice.")	
 	if dice == "":
 		print
@@ -16,53 +15,67 @@ def steps(total):
 """.format(total)	
 		print out
 		return total
-
+	
 def traps():
-	total= steps(total)
+	total = steps(0)
+
 	if total == float(1):
 		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
+		return steps(0)
 		return steps(total)
 
 	if total == float(5):
 		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
+		return steps(0)
 		return steps(total)
 
 	if total == float(6):
 		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
+		return steps(0)
 		return steps(total)
 
 	if total == float(52):
 		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
+		return steps(0)
 		return steps(total)
 
 	if total == float(84):
 		print "You stepped on a snake and the snake bite you. You will start from the beginning again."
+		return steps(0)
 		return steps(total)
 
 	elif total == float(3):
 		print "You stepped on a banana peel. You will slip 3 steps back." 
-		return abs(float(sub(total-3)))
+		return abs(float(total-3))
+		return steps(total)
 
 	elif total == float(28):
 		print "You found an escalator in front of you. You will skip 4 steps to the front."
-		return abs(float(add(total+4)))
+		return abs(float(total+4))
+		return steps(total)
 	
 	elif total == float(8):
 		print "You found an escalator in front of you. You will skip 4 steps to the front."
-		return abs(float(add(total+4)))
+		return abs(float(total+4))
+		return steps(total)
 	
 	elif total == float(78):
 		print "You found an escalator in front of you. You will skip 4 steps to the front."
-		return abs(float(add(total+4)))
+		return abs(float(total+4))
+		return steps(total)
 	
 	elif total == float(38):
 		print "You found an escalator in front of you. You will skip 4 steps to the front."
-		return abs(float(add(total+4)))
+		return abs(float(total+4))
+		return steps(total)
 	
 	elif total == float(32):
 		print "You found an escalator in front of you. You will skip 4 steps to the front."
-		return abs(float(add(total+4)))
+		return abs(float(total+4))
+		return steps(total)
 	
+	else:
+		return steps(total)
 
 def main():
 	total = steps(0)
